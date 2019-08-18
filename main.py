fd73366e31a99fcb9c06213d8c8306512ac624a2
arunp <arunp@arunp-m-93g6.hsd1.ca.comcast.net>
2019-08-17 17:32:37 -0700

    version 1

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     def step_thru_code():
         import argparse
 
-        verbose, asc = False, True
+        verbose, desc = False, False
         text = 'Some helper message about the program'
         # initiate the parser
         parser = argparse.ArgumentParser()
@@ -16,30 +16,29 @@ def main():
                             help='sort in descending order')
         args = parser.parse_args()
         if args.verbose: verbose = True
-        if args.descending: asc = False
-        return(verbose, asc)
+        if args.descending: desc = True
+        return(verbose, desc)
 
     # list of sort algorithms saved independently
     import bubble_sort as b
 
     '''
-    boring stuff list to sort, capturing verbosity level, and setting a timer
+    boring stuff: list to sort, capturing verbosity level, and setting a timer
     '''
     import time     # get a sense of the time taken
     A = [3, 14, 27, 12, 31, -6, 945643, 95, 0, 18, 5, 3, 980]
-    verbose, asc = step_thru_code()
+    verbose, desc = step_thru_code()
     print("unsorted   = ", A)
     t = time.time()         # get time just before the main routine
 
 
     ######## tweak code / options below  ###########
-    A = b.bubble_sort(A, verbose, asc)
-
+    A = b.bubble_sort(A, verbose, desc)
 
     ########## end of modifiable section ###########
 
     '''
-    more boring stuff about recording the time, printing the output result, and the time taken
+    more boring stuff: record the time, print the output result, and the time taken
     '''
     t = time.time() - t             # and now, after the routine
     print("sorted     = ", A)       # we want to see the output, don't we!?
